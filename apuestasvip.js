8e59caca82d19b21fca600c12ec115478df9eb54
netfreeplus <137662776+netfreeplus@users.noreply.github.com>
2024-03-10 22:18:16 -0500

    Update apuestasvip.js

--- apuestasvip.js
+++ apuestasvip.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Objeto con usuarios y contraseñas, cada uno con su fecha de vencimiento
     var users = {
         "Sebas": { "password": "Admin", "expiryDate": new Date("2024-03-31") },
+        "Santi": { "password": "1029", "expiryDate": new Date("2024-03-31") },
         "Santi1208": { "password": "10299837321", "expiryDate": new Date("2024-04-01") }
         // Agrega más usuarios y fechas de vencimiento según sea necesario
     };
